d784f3105cf3105a1354d74f1a6e1cd99f6e5777
Denise Baker <denviol@yahoo.com>
2019-09-20 09:28:38 -0500

    color change attempt

--- main.js
+++ main.js
@@ -17,21 +17,33 @@ const toFahrenheit =  (celsius) => {
 
 const button = document.getElementById("convert-button");
 
+// const celsiusColor = (x) => {
+//    x = toCelsius().value;
+//     let result = document.getElementById('output-box').value;
+//     if (x < 0) {
+//         result.style.color ='#2196f3';
+//     } else if (x > 32) {
+//         result.style.color ='#d82525';
+//     } else {
+//         result.style.color = '#219e5f'
+//     }
+
+// }
 
 
 
 const determineConverter = (e) => {
     const inputValue = document.getElementById('input').value;
-console.log(inputValue);
+
     const F = document.getElementById('F');
     const C = document.getElementById('C');
     let radioButton = document.getElementsByClassName('radio-button');
     if (C.checked) {
-        printToDom(toCelsius(inputValue), 'output-box')
+       printToDom(toCelsius(inputValue), 'output-box')
+        
     } else if (F.checked) {
         printToDom(toFahrenheit(inputValue), 'output-box')
     }
-    console.log("radionButton", radioButton, "C", C.checked, "F", F.checked,'inputValue', inputValue);
 }
 
 // Assign a function to be executed when the button is clicked
@@ -51,3 +63,44 @@ const findButtonArray = (buttonArr) => {
 
 }
 findButtonArray(buttonGroup)
+ //let output = document.getElementById('output-box');
+// let x = output;
+// console.log(x);
+// const C = document.getElementById('C');
+// y = output.innerHTML;
+//     console.log(y);
+
+
+//     const changeTextColorCelsius = (x, y) => {
+//     let output = document.getElementById('output-box');
+//     const F = document.getElementById('F');
+//     const C = document.getElementById('C');
+//     x = C.checked;
+    
+//     if (x == true && y > 32) {
+        
+//         output.style.color = '#d82525';
+//     } else if (x == true & y < 0) {
+//         output.style.color = '#2196f3';
+//     } else {
+//         output.style.color = '#219e5f';
+//     }
+// }
+ 
+    output = document.getElementById('output-box');
+    output.addEventListener('DOMContentLoaded', (e) => {
+        const F = document.getElementById('F');
+        const C = document.getElementById('C');
+        let x = C.checked;
+        let y = target.event.id;
+        console.log(y);
+
+        if (x == true && y > 32) {
+            
+            output.style.color = '#d82525';
+        } else if (x == true & y < 0) {
+            output.style.color = '#2196f3';
+        } else {
+            output.style.color = '#219e5f';
+        }
+    });
